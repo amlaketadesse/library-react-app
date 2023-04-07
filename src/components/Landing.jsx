ccf7c95dacf4e8a979c0daf24d2333ef7dd7e8e6
amlaketadesse <amlaketadesse@gmail.com>
2023-04-06 18:56:26 -0700

    styling

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import UndrawBooks from '../assets/Undraw_Books.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import UndrawBooks from "../assets/Undraw_Books.svg";
 
 const Landing = () => {
-    return (
-        <section id='landing'> 
-            <header>
-                <div className="header__container">
-                    <div className="header__description">
-                        <h1>California's most awarded online library platform</h1>
-                        <h2>Find your dreambook with <span className="purple">Library</span></h2>
-                        <Link to="#features">
-                            <button className='btn'>Browse Books</button>
-                        </Link>
-                    </div>
-                    <figure className='header__img--wrapper'>
-                        <img src={UndrawBooks} alt="" />
-                    </figure>
-                </div>
-            </header>
-        </section>
-    );
-}
+  return (
+    <section id="landing">
+      <header>
+        <div className="header__container">
+          <div className="header__description">
+            <h1>California's most awarded online library platform</h1>
+            <h2>
+              Find your dreambook with <span className="purple">Library</span>
+            </h2>
+            <Link to="#features">
+              <Link to="/books" className="btn">
+                Browse Books
+              </Link>
+            </Link>
+          </div>
+          <figure className="header__img--wrapper">
+            <img src={UndrawBooks} alt="" />
+          </figure>
+        </div>
+      </header>
+    </section>
+  );
+};
 
-export default Landing
+export default Landing;
